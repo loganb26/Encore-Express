babe50f24033751628852b0e62d81f4980a3be22
Kentrail <123190388+Kentrail2@users.noreply.github.com>
2024-04-25 19:40:07 -0500

    Update PlaylistPage.xaml.cs

--- EncoreExpress/Pages/PlaylistPage.xaml.cs
+++ EncoreExpress/Pages/PlaylistPage.xaml.cs
@@ -1,9 +1,43 @@
-namespace EncoreExpress.Pages;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-public partial class PlaylistPage : ContentPage
+namespace EncoreExpress.Pages
 {
-	public PlaylistPage()
-	{
-		InitializeComponent();
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlaylistPage : ContentPage
+    {
+        public ObservableCollection<Song> Songs { get; set; }
+
+        public PlaylistPage()
+        {
+            InitializeComponent();
+
+            // Initialize your song list here
+            Songs = new ObservableCollection<Song>
+            {
+                new Song { Name = "Song 1", IsAddedToQueue = false },
+                new Song { Name = "Song 2", IsAddedToQueue = false },
+                // Add more songs as needed
+            };
+
+            PlaylistView.ItemsSource = Songs;
+        }
+
+        public void OnBrowseLocalSongsClicked(object sender, EventArgs e)
+        {
+            // TODO: Add your code here to browse local songs
+        }
+
+        public void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            // TODO: Add your code here to navigate back to the home page
+        }
+    }
+
+    public class Song
+    {
+        public string Name { get; set; }
+        public bool IsAddedToQueue { get; set; }
+    }
 }
