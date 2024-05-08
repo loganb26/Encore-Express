ad330e1fb9e812832ba7e9f38d0eadfc3b8d81b1
ashleysuthe <165728147+ashleysuthe@users.noreply.github.com>
2024-05-07 20:35:45 -0500

    Update MenuPage.xaml.cs

--- EncoreExpress/Pages/MenuPage.xaml.cs
+++ EncoreExpress/Pages/MenuPage.xaml.cs
@@ -1,9 +1,44 @@
+using EncoreExpress.Classes;
+using System.Collections.ObjectModel;
+
 namespace EncoreExpress.Pages;
 
 public partial class MenuPage : ContentPage
 {
+	public ObservableCollection<Song> songs;
 	public MenuPage()
 	{
+		
 		InitializeComponent();
+		LoadPrevSongs();
+
 	}
+	private void LoadPrevSongs()
+	{
+        songs = new ObservableCollection<Song>();
+        if (App.SongList.Count > 0)
+        {
+            foreach (Song song in App.SongList)
+            {
+                songs.Add(song);
+            }
+            noSongs.Text = "";
+            prevSongs.ItemsSource = songs;
+
+        }
+        else
+        {
+            noSongs.Text = "There are no previously played songs.";
+        }
+    }
+
+    private void GoToPlayer(object sender, EventArgs e)
+    {
+        App.Current.MainPage = new PlayerPage();
+    }
+
+    private void GoToPlaylist(object sender, EventArgs e)
+    {
+        App.Current.MainPage=new PlaylistPage();
+    }
 }
