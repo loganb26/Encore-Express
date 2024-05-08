45bac23828a4cee79dc2ddf429581d2581b49821
Aabriti Shrestha <55425391+aabriti58@users.noreply.github.com>
2024-05-08 01:06:44 -0500

    Update SettingsPage.xaml.cs

--- EncoreExpress/Pages/SettingsPage.xaml.cs
+++ EncoreExpress/Pages/SettingsPage.xaml.cs
@@ -1,77 +1,36 @@
-using CommunityToolkit.Maui.Views;
-
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
 
 namespace EncoreExpress.Pages
 {
     public partial class SettingPage : ContentPage
     {
-        private MediaElement mediaElement;
-
-        public SettingPage(MediaElement mediaElement)
+        public SettingPage()
         {
             InitializeComponent();
 
-            // Assign the passed mediaElement to the local variable
-            this.mediaElement = mediaElement;
-
-            // Populate playback speed options
-            PopulateSpeedOptions();
-
-            // Set initial values
-            speedPicker.SelectedIndex = 2; // Default speed (1.0x)
-            muteSwitch.IsToggled = false; // Default mute state (unmuted)
-
-            // Event handlers
-            speedPicker.SelectedIndexChanged += SpeedPicker_SelectedIndexChanged;
-            muteSwitch.Toggled += MuteSwitch_Toggled;
-            volumeSlider.ValueChanged += VolumeSlider_ValueChanged;
+            // Other initialization code
         }
 
-        private void PopulateSpeedOptions()
+        private void LightModeSwitch_Toggled(object sender, ToggledEventArgs e)
         {
-            speedPicker.Items.Add("0.5x");
-            speedPicker.Items.Add("0.75x");
-            speedPicker.Items.Add("1.0x");
-            speedPicker.Items.Add("1.25x");
-            speedPicker.Items.Add("1.5x");
-            speedPicker.Items.Add("2.0x");
-        }
-
-        private void SpeedPicker_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (mediaElement != null && speedPicker.SelectedIndex >= 0)
+            if (e.Value)
             {
-                double speed = GetSelectedSpeed();
-                mediaElement.Speed = speed;
+                // Switch to light mode
+                Application.Current.UserAppTheme = AppTheme.Light;
+            }
+            else
+            {
+                // Switch to dark mode
+                Application.Current.UserAppTheme = AppTheme.Dark;
             }
         }
 
-        private double GetSelectedSpeed()
-        {
-            string selectedSpeedText = speedPicker.SelectedItem.ToString().Replace("x", "");
-            return double.Parse(selectedSpeedText);
-        }
-
-        private void MuteSwitch_Toggled(object sender, ToggledEventArgs e)
-        {
-            if (mediaElement != null)
-            {
-                mediaElement.ShouldMute = e.Value;
-                mediaElement.Volume = e.Value ? 0 : volumeSlider.Value;
-            }
-        }
-
-        private void VolumeSlider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            if (mediaElement != null)
-            {
-                mediaElement.Volume = e.NewValue;
-            }
-        }
-
-        private async void GoBackToPlayerPage(object sender, EventArgs e)
+        private void GoBackToPlayerPage(object sender, EventArgs e)
         {
             App.Current.MainPage = new PlayerPage();
         }
+
+        // Other methods
     }
 }
